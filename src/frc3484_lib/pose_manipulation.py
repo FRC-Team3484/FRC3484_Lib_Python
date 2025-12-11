3034cbc4148286fdd78636cd014952f3a0d23924
Purgso <jakob@spurgat.net>
2025-12-10 20:05:50 -0500

    feat: pose manipulation reorg

---
+++ src/frc3484_lib/pose_manipulation.py
@@ -0,0 +1,8 @@
+from ._pose_manipulation.pose_manipulation import get_april_tag_poses, apply_offset_to_pose, apply_offsets_to_poses, get_nearest_pose
+
+__all__ = [
+    "get_april_tag_poses",
+    "apply_offset_to_pose",
+    "apply_offsets_to_poses",
+    "get_nearest_pose",
+]
