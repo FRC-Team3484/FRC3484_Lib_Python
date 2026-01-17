632fe996928677a571dc9f5589691e661006cc7e
nfoert <nofoert@gmail.com>
2026-01-17 13:25:10 -0500

    feat: Create motion.py file for easier imports

---
+++ src/frc3484/motion.py
@@ -0,0 +1,27 @@
+from .__lib.motion.angular_pos_motor import AngularPositionMotor
+from .__lib.motion.linear_pos_motor import LinearPositionMotor
+from .__lib.motion.power_motor import PowerMotor
+from .__lib.motion.velocity_motor import VelocityMotor
+
+from .__lib.datatypes.motion_datatypes import \
+    SC_LauncherSpeed, \
+    SC_PIDConfig, \
+    SC_SolenoidConfig, \
+    SC_DoubleSolenoidConfig, \
+    SC_AngularFeedForwardConfig, \
+    SC_LinearFeedForwardConfig, \
+    SC_MotorConfig
+
+__all__ = [
+    "AngularPositionMotor",
+    "LinearPositionMotor",
+    "PowerMotor",
+    "VelocityMotor",
+    "SC_LauncherSpeed",
+    "SC_PIDConfig",
+    "SC_SolenoidConfig",
+    "SC_DoubleSolenoidConfig",
+    "SC_AngularFeedForwardConfig",
+    "SC_LinearFeedForwardConfig",
+    "SC_MotorConfig"
+]
