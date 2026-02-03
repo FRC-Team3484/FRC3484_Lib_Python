7982f7dfafda8a3a42df9de130dc89f9625f99a2
Ethan DeArmond <eedearmond@gmail.com>
2026-02-02 20:27:36 -0500

    feat 0.0.0.0

--- src/frc3484/leds.py
+++ src/frc3484/leds.py
@@ -2,6 +2,7 @@ from .__lib.leds.colorstack import ColorStack
 from .__lib.leds.colorwave import ColorWave
 from .__lib.leds.fallingsand import FallingSand
 from .__lib.leds.fire import Fire
+from .__lib.leds.static import Static
 from .__lib.leds.utils import correct_gamma
 
 
