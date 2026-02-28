e05e70e9562520b1445d42c0d4f3cb51dba568bf
nfoert <nofoert@gmail.com>
2026-02-28 11:19:12 -0500

    fix(leds): Fix gamma again

--- src/frc3484/__lib/leds/utils.py
+++ src/frc3484/__lib/leds/utils.py
@@ -1,4 +1,4 @@
 from wpilib import Color
 
-def correct_gamma(self, color: Color, gamma: float) -> Color:
+def correct_gamma(color: Color, gamma: float) -> Color:
     return Color(color.red** gamma, color.green** gamma, color.blue** gamma)
